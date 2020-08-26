b31735cf6b1eebe67950d712dcc6ca0524f490d5
Becca <beccacauthorn@gmail.com>
2020-08-25 22:47:16 -0400

    glider

--- gameoflife.py
+++ gameoflife.py
@@ -31,39 +31,54 @@ class Application(tk.Frame):
         self.play.grid(row=4, column=27)
 
     def get_neighbors(self, row, col):
-        n1 = self.button_arr[0][1]
-        n2 = self.button_arr[1][1]
-        n3 = self.button_arr[1][0]
-        return [n1, n2, n3]
+        coords = [(row-1, col-1), (row-1, col), (row-1, col+1),
+                  (row  , col-1),               (row  , col+1),
+                  (row+1, col-1), (row+1, col), (row+1, col+1)]
+        neighbors = []
+        for r, c in coords:
+            if (r > -1 and r < 25) and (c > -1 and c < 25):
+                neighbors.append(self.button_arr[r][c])
+
+        return neighbors
 
     def on_play(self):
         for k in range(25):
             for f in range(25):
                 self.button_arr[k][f].config(state="disabled")
 
-        # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        bg_black_list = []
-        for n in self.get_neighbors(0, 0):
-            bg = n['bg']
-            bg_black_list.append(bg == 'black')
+        future_colors = []
+        for x in range(25):
+            future_colors.append([])
+            for y in range(25):
+                future_colors[-1].append("white")
 
-        # bg_black_list = [bg1 == 'black', bg2 == 'black', bg3 == 'black']
-        count_bg_black = bg_black_list.count(True)
+        # apply the rules and build a separate grid of future values
+        for r in range(25):
+            for c in range(25):
+                # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                bg_black_list = []
+                for n in self.get_neighbors(r, c):
+                    bg = n['bg']
+                    bg_black_list.append(bg == 'black')
 
-        if self.button_arr[0][0]['bg'] == 'black':
-            if count_bg_black >= 2 and count_bg_black <= 3:
-                self.button_arr[0][0].configure(bg="black")
-            else:
-                self.button_arr[0][0].configure(bg="white")
-        if self.button_arr[0][0]['bg'] == 'white':
-            if count_bg_black == 3:
-                self.button_arr[0][0].configure(bg="black")
+                # bg_black_list = [bg1 == 'black', bg2 == 'black', bg3 == 'black']
+                count_bg_black = bg_black_list.count(True)
 
-        for k in range(25):
-            for f in range(25):
-                self.button_arr[k][f].config(state="normal")
-        
-        
+                if self.button_arr[r][c]['bg'] == 'black':
+                    if count_bg_black >= 2 and count_bg_black <= 3:
+                        future_colors[r][c] = "black"
+                    else:
+                        future_colors[r][c] = "white"
+                if self.button_arr[r][c]['bg'] == 'white':
+                    if count_bg_black == 3:
+                        future_colors[r][c] = "black"
+
+        # using the newly built grid of values, change all colors at the same time
+        for r in range(25):
+            for c in range(25):
+                self.button_arr[r][c].configure(bg=future_colors[r][c])
+                self.button_arr[r][c].config(state="normal")
+
 
 
 root = tk.Tk()
